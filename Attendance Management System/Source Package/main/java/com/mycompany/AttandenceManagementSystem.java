eb55c8a948580dbfd80b4184e6e20e7bf78d99ed
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2024-10-25 12:32:48 +0530

    Add files via upload

---
+++ Attendance Management System/Source Package/main/java/com/mycompany/AttandenceManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.attandence.management.system;
+
+/**
+ *
+ * @author Saurav ratan
+ */
+public class AttandenceManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
