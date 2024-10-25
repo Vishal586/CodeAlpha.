6332321322607dab8499a4025f4bc16373b1b55b
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2024-10-25 12:23:25 +0530

    Add files via upload

---
+++ Attendance Management System/Source Package/main/java/com/Main/Connecction.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Main;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Saurav ratan
+ */
+class Connecction {
+
+    PreparedStatement prepareStatement(String insert_into_studenttb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
